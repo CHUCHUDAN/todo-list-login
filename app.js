80c34a45fce4f5b583be01981f8dce25f6a591d8
CHUCHUDAN <daniel870217@gmail.com>
2023-01-31 13:05:35 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars');
 const Todo = require('./models/todo')
+const bodyParser = require('body-parser') // 引用 body-parser
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -10,6 +11,8 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -22,6 +25,9 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
@@ -32,6 +38,16 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+app.get('/todos/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
 app.listen(3000, () => {
   console.log('The app is Listening on http://localhost:3000')
 })
