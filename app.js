6031243c8a1054b3cbae24621cdb6f0a04e0eb22
CHUCHUDAN <daniel870217@gmail.com>
2023-01-31 11:59:03 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars');
+const Todo = require('./models/todo')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -25,7 +26,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index', {todos}))
+    .catch(error => console.error(error))
 })
 
 app.listen(3000, () => {
