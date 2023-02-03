911ac911d803954e02d3c712c11b55718075b0c4
CHUCHUDAN <daniel870217@gmail.com>
2023-02-03 14:02:26 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override')
 
 const routes = require('./routes')// 引用路由器
 
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('./config/mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
