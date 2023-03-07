34f8d83294e12fadbdc8948ec02182e0d93ec815
CHUCHUDAN <daniel870217@gmail.com>
2023-03-07 12:20:12 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+const session = require('express-session')
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override')
@@ -12,14 +13,23 @@ require('./config/mongoose')
 //設定埠號
 const PORT = process.env.PORT || 3000
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
 
+
+
 // 將 request 導入路由器
 app.use(routes)
+
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
